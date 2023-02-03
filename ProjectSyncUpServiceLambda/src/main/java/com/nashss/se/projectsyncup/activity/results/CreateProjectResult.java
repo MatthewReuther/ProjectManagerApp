b99a9582f016db0451ef3bbeec8ce80c8635bd67
Matthew Reuther <matthew.reuther@gmail.com>
2023-02-03 17:03:55 -0600

    added basic createResult with java docs

--- ProjectSyncUpServiceLambda/src/main/java/com/nashss/se/projectsyncup/activity/results/CreateProjectResult.java
+++ ProjectSyncUpServiceLambda/src/main/java/com/nashss/se/projectsyncup/activity/results/CreateProjectResult.java
@@ -1,4 +1,32 @@
 package com.nashss.se.projectsyncup.activity.results;
 
+import com.nashss.se.projectsyncup.models.ProjectModel;
+
+/**
+ * The `CreateProjectResult` class represents the result of creating a projectModel.
+ * It contains the projectModel that was created.
+ */
 public class CreateProjectResult {
+    /**
+     * The projectModel that was created.
+     */
+    private final ProjectModel projectModel;
+
+    /**
+     * Private constructor to enforce the use of the Builder class.
+     *
+     * @param projectModel The projectModel that was created.
+     */
+    private CreateProjectResult(ProjectModel projectModel) {
+        this.projectModel = projectModel;
+    }
+
+    /**
+     * Returns the projectModel that was created.
+     *
+     * @return The projectModel that was created.
+     */
+    public ProjectModel getProjectModel() {
+        return projectModel;
+    }
 }
