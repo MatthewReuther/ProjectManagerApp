b73f44a9552a6e3895bed19ceee4a90d76b4275b
Matthew Reuther <matthew.reuther@gmail.com>
2023-02-06 15:12:25 -0600

    minor spacing issues fixed

--- ProjectSyncUpServiceLambda/src/main/java/com/nashss/se/projectsyncup/exceptions/ProjectNotFoundException.java
+++ ProjectSyncUpServiceLambda/src/main/java/com/nashss/se/projectsyncup/exceptions/ProjectNotFoundException.java
@@ -5,39 +5,39 @@ package com.nashss.se.projectsyncup.exceptions;
  */
 public class ProjectNotFoundException extends RuntimeException {
 
-        private static final long serialVersionUID = -912326717789387971L;
+    private static final long serialVersionUID = -912326717789387971L;
 
-        /**
-         * Exception with no message or cause.
-         */
-        public ProjectNotFoundException() {
-            super();
-        }
-
-        /**
-         * Exception with a message, but no cause.
-         * @param message A descriptive message for this exception.
-         */
-        public ProjectNotFoundException(String message) {
-            super(message);
-        }
-
-        /**
-         * Exception with no message, but with a cause.
-         * @param cause The original throwable resulting in this exception.
-         */
-        public ProjectNotFoundException(Throwable cause) {
-            super(cause);
-        }
-
-        /**
-         * Exception with message and cause.
-         * @param message A descriptive message for this exception.
-         * @param cause The original throwable resulting in this exception.
-         */
-        public ProjectNotFoundException(String message, Throwable cause) {
-            super(message, cause);
-        }
+    /**
+     * Exception with no message or cause.
+     */
+    public ProjectNotFoundException() {
+        super();
     }
 
+    /**
+     * Exception with a message, but no cause.
+     * @param message A descriptive message for this exception.
+     */
+    public ProjectNotFoundException(String message) {
+        super(message);
+    }
+
+    /**
+     * Exception with no message, but with a cause.
+     * @param cause The original throwable resulting in this exception.
+     */
+    public ProjectNotFoundException(Throwable cause) {
+        super(cause);
+    }
+
+    /**
+     * Exception with message and cause.
+     * @param message A descriptive message for this exception.
+     * @param cause The original throwable resulting in this exception.
+     */
+    public ProjectNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
+
+
