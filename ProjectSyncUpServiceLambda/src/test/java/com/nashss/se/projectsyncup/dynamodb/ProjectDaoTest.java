499ab0fc2aa4faa00bcb5440e5833758e0964c7e
Matthew Reuther <matthew.reuther@gmail.com>
2023-02-07 21:29:07 -0600

    added unit test for ProjectDao

--- ProjectSyncUpServiceLambda/src/test/java/com/nashss/se/projectsyncup/dynamodb/ProjectDaoTest.java
+++ ProjectSyncUpServiceLambda/src/test/java/com/nashss/se/projectsyncup/dynamodb/ProjectDaoTest.java
@@ -1,2 +1,40 @@
-package com.nashss.se.projectsyncup.dynamodb;public class ProjectDaoTest {
+package com.nashss.se.projectsyncup.dynamodb;
+
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.nashss.se.projectsyncup.dynamodb.models.Project;
+import com.nashss.se.projectsyncup.utils.ProjectSyncUpServiceUtils;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.verify;
+import static org.mockito.MockitoAnnotations.openMocks;
+
+public class ProjectDaoTest {
+    @Mock
+    private DynamoDBMapper dynamoDBMapper;
+
+    private ProjectDao projectDao;
+
+    @BeforeEach
+    public void setup() {
+        openMocks(this);
+        projectDao = new ProjectDao(dynamoDBMapper);
+    }
+    @Test
+    public void getProject_withProjectId_callsMapperWithPartitionKey() {
+        // GIVEN
+        String projectId = ProjectSyncUpServiceUtils.generateProjectId();
+        when(dynamoDBMapper.load(Project.class, projectId)).thenReturn(new Project());
+
+        // WHEN
+        Project project = projectDao.getProject(projectId);
+
+        // THEN
+        assertNotNull(project);
+        verify(dynamoDBMapper).load(Project.class, projectId);
+
+    }
 }
