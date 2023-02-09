1e8fa58f13f28cf78eb596ce96c17707110bfcf8
Matthew Reuther <matthew.reuther@gmail.com>
2023-02-09 14:29:15 -0600

    added CreateRequestTest and create curl command

---
+++ ProjectSyncUpServiceLambda/src/test/java/com/nashss/se/projectsyncup/activity/request/CreateProjectRequestTest.java
@@ -0,0 +1,48 @@
+package com.nashss.se.projectsyncup.activity.request;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.nashss.se.projectsyncup.activity.requests.CreateProjectRequest;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CreateProjectRequestTest {
+
+
+    @Test
+    public void testJsonDeserialize() throws IOException {
+        // Define the expected values
+        String expectedProjectName = "Test Project";
+        String expectedProjectDescription = "This is a test project.";
+        String expectedProjectStatus = "Active";
+        String expectedCreatedById = "user1";
+        List<String> expectedProjectTasks = Arrays.asList("Task 1", "Task 2");
+        List<String> expectedProjectMembers = Arrays.asList("user2", "user3");
+
+        // Define the JSON string
+        String json = "{" +
+                "\"projectName\":\"" + expectedProjectName + "\"," +
+                "\"projectDescription\":\"" + expectedProjectDescription + "\"," +
+                "\"projectStatus\":\"" + expectedProjectStatus + "\"," +
+                "\"createdById\":\"" + expectedCreatedById + "\"," +
+                "\"projectTasks\":[\"" + String.join("\",\"", expectedProjectTasks) + "\"]," +
+                "\"projectMembers\":[\"" + String.join("\",\"", expectedProjectMembers) + "\"]" +
+                "}";
+
+        // Use the ObjectMapper to deserialize the JSON string
+        ObjectMapper mapper = new ObjectMapper();
+        CreateProjectRequest request = mapper.readValue(json, CreateProjectRequest.class);
+
+        // Assert that the deserialized values match the expected values
+        assertEquals(expectedProjectName, request.getProjectName());
+        assertEquals(expectedProjectDescription, request.getProjectDescription());
+        assertEquals(expectedProjectStatus, request.getProjectStatus());
+        assertEquals(expectedCreatedById, request.getCreatedById());
+        assertEquals(expectedProjectTasks, request.getProjectTasks());
+        assertEquals(expectedProjectMembers, request.getProjectMembers());
+    }
+}
---
+++ resources/curl-commands.md
@@ -0,0 +1,20 @@
+## CreateProject
+```
+curl -X POST \
+    http://127.0.0.1:3000/projects \
+    -H 'Authorization: Bearer eyJraWQiOiJJV1Y0N1BkbTdRN091b1pDdm5YdmVLVGZVQ1wvcTFwNkUxNGJQcVJydzV4dz0iLCJhbGciOiJSUzI1NiJ9.eyJhdF9oYXNoIjoiV3RiN1RaV1dxRXU0YVZuTTZNbmdnZyIsInN1YiI6IjdiMThiYjM5LTFkY2MtNDc4Zi1iODVjLWI4ODUyMTAxMGVkZCIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJpc3MiOiJodHRwczpcL1wvY29nbml0by1pZHAudXMtZWFzdC0yLmFtYXpvbmF3cy5jb21cL3VzLWVhc3QtMl90YWN4RkNLMEkiLCJjb2duaXRvOnVzZXJuYW1lIjoiN2IxOGJiMzktMWRjYy00NzhmLWI4NWMtYjg4NTIxMDEwZWRkIiwib3JpZ2luX2p0aSI6ImU0YmE0N2QxLWNiMzItNGEzMC05NzZjLWVjMjEwZWEyM2I4ZiIsImF1ZCI6IjVlZG1mOGQyb2M2NGhkb2xrZ2hjdWQzNWd2IiwiZXZlbnRfaWQiOiI3NjlkZDFiMi1lMzViLTRiZjEtOTk3NC1lZTUxZjNjMTg1MjAiLCJ0b2tlbl91c2UiOiJpZCIsImF1dGhfdGltZSI6MTY3NTk2MjI3MCwibmFtZSI6Ik1hdHRoZXciLCJleHAiOjE2NzU5NjU4NzAsImlhdCI6MTY3NTk2MjI3MCwianRpIjoiNjdlMDEzOTMtODZkOS00NjYwLTk1MGEtOTk1ZjNmMGQ3Y2Y2IiwiZW1haWwiOiJtYXR0aGV3LnJldXRoZXJAZ21haWwuY29tIn0.JyCXkoGkWcT-HIaQzLTG1v4e781BEHmTmLOAFP5tzeW_kC-zmasvpKf4TzbgfX3HZk5lOVaQ5zEZmcC-AI4NwY0EmItiO0jo-9-BN_P6VKsjJpRu9R2bLsGMeZcqSkr1qTMIVBzZisJOC8T-4myx0Z8P5hiIq1Y8vtg06w2lr4-xc7b6kgQcbAcB9g_Z1AYzirznilP1YMPRxKQufYUNlsuj5CM5AUzhq-lE2p7eXl2n3bAiLnaHYthQxabJfeSG_jaAxLugp1ScqpwJiYOwNdrS27dpPb1uMjPg8iphpIklMOLIOZOLTALTfoDIqxODKakOHRSge_szmh9i-tv_qA' \
+    -d '{
+    "projectName": "Test Project",
+    "projectDescription": "This is a test project",
+    "projectStatus": "ACTIVE",
+    "createdById": "1234",
+    "projectTasks": [
+    "Task 1",
+    "Task 2"
+    ],
+    "projectMembers": [
+    "User 1",
+    "User 2"
+    ]
+}'
+```
