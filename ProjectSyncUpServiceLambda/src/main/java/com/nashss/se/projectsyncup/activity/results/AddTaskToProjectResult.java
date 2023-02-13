b026e9a6c1c02afc8371847117e294cb55906814
Matthew Reuther <matthew.reuther@gmail.com>
2023-02-13 13:39:43 -0600

    added addTaskResult class

--- ProjectSyncUpServiceLambda/src/main/java/com/nashss/se/projectsyncup/activity/results/AddTaskToProjectResult.java
+++ ProjectSyncUpServiceLambda/src/main/java/com/nashss/se/projectsyncup/activity/results/AddTaskToProjectResult.java
@@ -1,4 +1,65 @@
 package com.nashss.se.projectsyncup.activity.results;
 
+import com.nashss.se.projectsyncup.dynamodb.models.TaskModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * The `AddTaskToProjectResult` class represents the result of adding a TaskModel.
+ * It contains the projectModel that was created.
+ */
 public class AddTaskToProjectResult {
+
+    private final List<TaskModel> taskList;
+
+    /**
+     * Private constructor to enforce the use of the Builder class.
+     *
+     * @param taskList The list of tasks in project.
+     */
+    private AddTaskToProjectResult(List<TaskModel> taskList) {
+        this.taskList = taskList;
+    }
+
+    /**
+     * Returns the taskList that was updated.
+     *
+     * @return The taskList that was updated.
+     */
+    public List<TaskModel> getTaskList() {
+        return new ArrayList<>(taskList);
+    }
+
+    /**
+     * Returns a string representation of the {@link AddTaskToProjectResult} object.
+     *
+     * @return a string representation of the {@link AddTaskToProjectResult} object, in the format of
+     * "AddTaskToProjectResult{task=TASK_MODEL}".
+     */
+    @Override
+    public String toString() {
+        return "AddTaskToProjectResult{" +
+                "taskList=" + taskList +
+                '}';
+    }
+
+    //CHECKSTYLE:OFF:Builder
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public static class Builder {
+        private List<TaskModel> taskList;
+
+        public Builder withTaskList(List<TaskModel> taskList) {
+            this.taskList = new ArrayList<>(taskList);
+            return this;
+        }
+
+        public AddTaskToProjectResult build() {
+            return new AddTaskToProjectResult(taskList);
+        }
+    }
+
 }
