cc45862efee31ec641e25ee67b7c2dcb90bf7d71
Matthew Reuther <matthew.reuther@gmail.com>
2023-02-18 17:16:54 -0600

    added basic template for RemoveTaskLambda and activity classes

---
+++ ProjectSyncUpServiceLambda/src/main/java/com/nashss/se/projectsyncup/lambda/RemoveTaskFromProjectLambda.java
@@ -0,0 +1,46 @@
+package com.nashss.se.projectsyncup.lambda;
+
+import com.nashss.se.projectsyncup.activity.requests.RemoveTaskFromProjectRequest;
+import com.nashss.se.projectsyncup.activity.requests.RemoveTaskFromProjectResult;
+import com.nashss.se.projectsyncup.dynamodb.ProjectDao;
+import com.nashss.se.projectsyncup.dynamodb.models.Project;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javax.inject.Inject;
+import java.util.List;
+
+public class RemoveTaskFromProjectLambda {
+    private final Logger log = LogManager.getLogger();
+    private final ProjectDao projectDao;
+
+
+    /**
+     *
+     * @param projectDao PlaylistDao to access the project table.
+     */
+    @Inject
+    public RemoveTaskFromProjectLambda(ProjectDao projectDao) {
+        this.projectDao = projectDao;
+    }
+
+    /**
+     *
+     * @param request request containing information on a task to be removed
+     * @return the task being removed from the project
+     */
+    public RemoveTaskFromProjectResult handleRequest(final RemoveTaskFromProjectRequest request) {
+        log.info("Received RemoveTaskFromProjectRequest {} ", request);
+
+        String taskId = request.getTaskId();
+        String projectId = request.getprojectId();
+
+        Project project = projectDao.getProject(projectId);
+
+        projectDao.saveProject(project);
+
+        return RemoveTaskFromProjectResult.builder()
+                .build();
+    }
+
+}
