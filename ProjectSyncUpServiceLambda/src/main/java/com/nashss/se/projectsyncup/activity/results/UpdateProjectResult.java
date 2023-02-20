0f7a7b70b9332da390f306af1a07c9a7dbed7484
Matthew Reuther <matthew.reuther@gmail.com>
2023-02-19 21:25:33 -0600

    implmented builder and getter method in UpdateProjectResult

--- ProjectSyncUpServiceLambda/src/main/java/com/nashss/se/projectsyncup/activity/results/UpdateProjectResult.java
+++ ProjectSyncUpServiceLambda/src/main/java/com/nashss/se/projectsyncup/activity/results/UpdateProjectResult.java
@@ -1,4 +1,59 @@
 package com.nashss.se.projectsyncup.activity.results;
 
-public class UpdateProjectResult {
+import com.nashss.se.projectsyncup.models.ProjectModel;
+
+public class updated {
+
+    /**
+     * The projectModel that was updated.
+     */
+    private final ProjectModel project;
+
+    /**
+     * Private constructor to enforce the use of the Builder class.
+     *
+     * @param project The projectModel that was crupdatedeated.
+     */
+    private UpdateProjectResult(ProjectModel project) {
+        this.project = project;
+    }
+
+    /**
+     * Returns the projectModel that was updated.
+     *
+     * @return The projectModel that was updated.
+     */
+    public ProjectModel getProject() {
+        return project;
+    }
+
+    /**
+     * Returns a string representation of the {@link UpdateProjectResult} object.
+     *
+     * @return a string representation of the {@link UpdateProjectResult} object, in the format of
+     * "UpdateProjectResult{project=PROJECT_MODEL}".
+     */
+    @Override
+    public String toString() {
+        return "UpdateProjectResult{" +
+                "project=" + project +
+                '}';
+    }
+
+    //CHECKSTYLE:OFF:Builder
+    public static Builder builder() {
+        return new Builder();
+    }
+    public static class Builder {
+        private ProjectModel project;
+
+        public Builder withProject(ProjectModel project) {
+            this.project = project;
+            return this;
+        }
+
+        public UpdateProjectResult build() {
+            return new UpdateProjectResult(project);
+        }
+    }
 }
